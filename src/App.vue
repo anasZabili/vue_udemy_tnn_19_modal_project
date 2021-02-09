38ce676be63dcfe9657c31d399b2391d95627374
anas zabili <anaszab78@gmail.com>
2021-02-09 21:37:11 +0100

    lesson 25

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ this.title }}</h1>
+  <Modal :header="header" :text="text" theme="sale" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      title: "My first vue app",
+      header: "Sign up for the Giveawey !",
+      text: "Grab your item",
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +29,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
